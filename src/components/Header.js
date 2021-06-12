2bf90a04188113ecaa2005901e05798c47e94a54
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-06-12 10:31:40 -0400

    another one

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,11 +14,11 @@ function Header({loggedInUser, setLoggedInUser}) {
             <img src="https://i.imgur.com/fwSD2s0.png" alt="game-on" style={{ height: "70px" }} />
             {/* <h2 className="nav-elements">GAME ON</h2> */}
             <div className="nav-btns-container">
-                { loggedInUser ? <NavLink to="/games" className="nav-elements"><img src="https://i.imgur.com/PiITIgg.png" alt="games" style={{ height: "65px"}}/></NavLink> : null}
-                { loggedInUser ? <NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/gq5Ra6O.png" alt="find-groups" style={{ height: "65px"}}/></NavLink> : null}
-                { loggedInUser ? <NavLink to={`/creategroup`} className="nav-elements"><img src="https://i.imgur.com/Mk9MGYC.png" alt="create-group" style={{ height: "65px"}}/></NavLink> : null}
-                { loggedInUser ? <NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/sMSegvD.png" alt="notifications" style={{ height: "60px"}}/></NavLink> : null}
-                { loggedInUser ? <NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/wpZRMeJ.png" alt="messages" style={{ height: "65px"}}/></NavLink> : null}
+                { loggedInUser ? <span className="icons"><NavLink to="/games" className="nav-elements"><img src="https://i.imgur.com/PiITIgg.png" alt="games" style={{ height: "65px"}}/></NavLink></span> : null}
+                { loggedInUser ? <span className="icons"><NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/gq5Ra6O.png" alt="find-groups" style={{ height: "65px"}}/></NavLink></span> : null}
+                { loggedInUser ? <span className="icons"><NavLink to={`/creategroup`} className="nav-elements"><img src="https://i.imgur.com/Mk9MGYC.png" alt="create-group" style={{ height: "65px"}}/></NavLink></span> : null}
+                { loggedInUser ? <span className="icons"><NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/sMSegvD.png" alt="notifications" style={{ height: "65px"}}/></NavLink></span> : null}
+                { loggedInUser ? <span className="icons"><NavLink to={`/profile/${loggedInUser.id}`} className="nav-elements"><img src="https://i.imgur.com/wpZRMeJ.png" alt="messages" style={{ height: "65px"}}/></NavLink></span> : null}
                 { loggedInUser ? <NavLink to={`/profile/${loggedInUser.id}`}><img id="profile-badge-nav" src={loggedInUser.profile_pic} alt={loggedInUser.username} /></NavLink> : null}
                 { loggedInUser ? <NavLink to="/" onClick={handleClick} className="nav-elements log">Log Out</NavLink> : <NavLink to="/" className="nav-elements log">Log In</NavLink>}
             </div>
